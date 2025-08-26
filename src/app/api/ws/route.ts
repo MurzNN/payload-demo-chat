5c845b5a18f1bc73a9c7656f670a2e992662defe
Alexey Murz Korepov <MurzNN@gmail.com>
2025-08-26 10:16:24 +0400

    Upgrade to next-ws@beta to hanlde Next.js headers natively

--- src/app/api/ws/route.ts
+++ src/app/api/ws/route.ts
@@ -1,7 +1,5 @@
-import { getContainer } from '@/container'
 import config from '@payload-config'
-import type { IncomingMessage } from 'node:http'
-import { AuthResult } from 'node_modules/payload/dist/auth/operations/auth'
+import { headers } from 'next/headers'
 import { getPayload } from 'payload'
 
 export function GET() {
@@ -11,33 +9,12 @@ export function GET() {
   return new Response('Upgrade Required', { status: 426, headers })
 }
 
-export function SOCKET(
+export async function UPGRADE(
   client: import('ws').WebSocket,
-  request: import('http').IncomingMessage,
   server: import('ws').WebSocketServer,
-  context: { params: Record<string, string | string[]> },
+  request: import('next/server').NextRequest,
+  context: import('next-ws/server').RouteContext<'/api/ws'>,
 ) {
-  payloadAuthByHttpRequest(request).then(async ({ user }) => {
-    const container = await getContainer()
-    container.cradle.webSocketManager.handleConnection({
-      client,
-      request,
-      server,
-      user,
-    })
-  })
-}
-
-async function payloadAuthByHttpRequest(request: IncomingMessage): Promise<AuthResult> {
-  // We need to convert Node.js headers to the Next.js headers format.
-  const headers = new Headers()
-  Object.entries(request.headers).forEach(([key, value]) => {
-    headers.set(key, value as string)
-  })
-  const payload = await getPayload({ config })
-  const authResult = await payload.auth({
-    canSetHeaders: false,
-    headers,
-  })
-  return authResult
+  await headers()
+  const payload = await getPayload({ config }) // HERE IS THE PROBLEM!!!
 }
